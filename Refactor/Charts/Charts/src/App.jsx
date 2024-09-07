40e20fef88413db71a3b773abf3559ad680c2e45
Andy <66146370+andosgee@users.noreply.github.com>
2024-09-07 18:44:50 +1200

    Created Pie Chart

---
+++ Refactor/Charts/Charts/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import Chart from './GraphModel/GraphModel.jsx';
+import './App.css';
+
+function App() {
+    return (
+      <div>
+      <h1>CSV Data Chart</h1>
+      <Chart />
+    </div>
+    );
+}
+export default App;
